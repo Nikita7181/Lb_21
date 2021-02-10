f16a67461da52df25fa59b503a745dccab697d97
Никита <70635031+Nikita7181@users.noreply.github.com>
2021-02-10 23:34:46 +0300

    Update String.h

--- String.h
+++ String.h
@@ -8,7 +8,7 @@ private:
 public:
     char* get_str();
     int Size(const char* str);// определение длинны строки, функция класса
-    int Size();
+    int Size();// пользовательская функция(выполняется на объекте класаа)
     String();// конструктор по умолчанию
     String(const char* ob);// конструктор с параметрами
     String( String& ob);//конструктор копирования
@@ -16,11 +16,10 @@ public:
     void replacement(char* My, char* My1);// функция замены симфолов
     char operator[](int index);// получение по индексу
     bool operator==(String& My);// сравнение
-    String & operator=(String& My);// присваивание
+    String& operator=(String& My);// присваивание
     ~String();//деструктор
-    String & operator+( String& My2);// инкапсуляция
+    String& operator+( String& My2);// инкапсуляция
 };
-
 
 std::ostream& operator<<(std::ostream& is, String& My);//вывод
 std::istream& operator>>(std::istream& os, String& My);//ввод
