8cee043b984abe4786f924b2993bc083ded12e7e
Никита <70635031+Nikita7181@users.noreply.github.com>
2021-02-11 00:00:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,16 @@ int main()
     String m("t");
     String s;
     String l(k);
+    
     std::cout << l << std::endl;
     std::cout << "Size: " <<k.Size() << std::endl;
     std::cout << k << std::endl;
     std::cout << k.Search(m)<<std::endl;
+    
     k.replacement("t", "k");
+    
     std::cout << k << std::endl;
+    
     if(k==m)
     {
         std::cout << "Yes" << std::endl;
@@ -20,6 +24,7 @@ int main()
     {
         std::cout << "No" << std::endl;
     }
+    
     s = k+m;
     std::cout << s << std::endl;
     return 0;
