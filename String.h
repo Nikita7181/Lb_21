48a4a87cbb8523de76388964ffe9d6f705a13927
Никита <70635031+Nikita7181@users.noreply.github.com>
2021-02-14 00:55:45 +0300

    Update String.h

--- String.h
+++ String.h
@@ -12,7 +12,8 @@ public:
     String();// конструктор по умолчанию
     String(const char* ob);// конструктор с параметрами
     String( String& ob);//конструктор копирования
-    int Search(String& My);// поиск подстроки
+    int Search(char My);// поиск подстроки
+    int Search(char* My);
     void replacement(String My, String My1);// функция замены симфолов
     char operator[](int index);// получение по индексу
     bool operator==(String& My);// сравнение
@@ -21,6 +22,6 @@ public:
     String& operator+( String& My2);// инкапсуляция
 };
 
-std::ostream& operator<<(std::ostream& is, String& My);//вывод
+std::ostream& operator<<(std::ostream& os, String& My);// вывод
 std::istream& operator>>(std::istream& os, String& My);//ввод(в разработке)
 
