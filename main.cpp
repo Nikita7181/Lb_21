832600ef405e1708434fde95400516a32e20e0d7
Никита <70635031+Nikita7181@users.noreply.github.com>
2021-02-14 00:56:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,37 +2,22 @@
 #include "String.h"
 int main()
 {
-    String k("aaatggtg");
-    String m("t");
-    String s;
-    String l(k);
-    String My;
-    
-    std::cout << l << std::endl;
-    std::cout << "Size: " <<k.Size() << std::endl;
-    std::cout << k << std::endl;
-    std::cout << k.Search(m)<<std::endl;
-    
-    k.replacement( "t",  "k");
-    
-    std::cout << k << std::endl;
-    
-    if(k==m)
-    {
-        std::cout << "Yes" << std::endl;
-    }
-    else
-    {
-        std::cout << "No" << std::endl;
-    }
-    
-    s = k+m;
-    
-    std::cout << s << std::endl;
-    std::cout << s.Size()<< std::endl;
-    std::cout << "Enter: ";
-    std::cin >> My;
-    std::cout << My << std::endl;
-    std::cout << My.Size() << std::endl;
+    String test("abcefghrtasasafsafsafsafaffafs");
+    std::cout << test << std::endl;
+    String test_2(test);
+    String test_3;
+    String test_4;
+    std::cout << test_2 << std::endl;
+    std::cout << "Size: " <<test.Size() << std::endl;
+    std::cout << test.Search('b')<<std::endl;
+    std::cout << test.Search("ffa")<<std::endl;
+    test.replacement( "a",  "l");
+   std::cout << test << std::endl;
+   test_3 = test_2 + test;
+   std::cout << test_3 << std::endl;
+    std::cin >> test_4;
+    std::cout << test_4.Size() << std::endl;
+    std::cout << test_4 << std::endl;
+   
     return 0;
 }
