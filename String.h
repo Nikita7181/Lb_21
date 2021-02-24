fb2c9bc45f1ab4a32423862164908e5c8816835b
Никита <70635031+Nikita7181@users.noreply.github.com>
2021-02-25 00:37:08 +0300

    Update String.h

--- String.h
+++ String.h
@@ -1,20 +1,22 @@
 #pragma once
+
 #include <fstream>
+#include <iostream>
 
 class String
 {
 private:
     char* str;
-    int Size(const char* str);// определение длинны строки, функция класса
 public:
-    char* get_str();// геттер
+    int Size(const char* str);// определение длинны строки, функция класса
+    const char* get_str();// геттер
     int Size();// пользовательская функция(выполняется на объекте класаа)
     String();// конструктор по умолчанию
     String(const char* ob);// конструктор с параметрами
     String( String& ob);//конструктор копирования
-    int Search(char My);// поиск подстроки
-    int Search(char* My);
-    void replacement(String My, String My1);// функция замены симфолов
+    int Search(char My);// поиск символа
+    int Search(char* My);// перергрузка для подстроки
+    void replacement(char My, char My1);// функция замены симфолов
     char operator[](int index);// получение по индексу
     bool operator==(String& My);// сравнение
     String& operator=(String& My);// присваивание
@@ -22,6 +24,7 @@ public:
     String& operator+( String& My2);// инкапсуляция
 };
 
+//String& operator+( String& My1, String& My2);// инкапсуляция
 std::ostream& operator<<(std::ostream& os, String& My);// вывод
 std::istream& operator>>(std::istream& os, String& My);//ввод(в разработке)
 
